b260f39b23918a553bf3d9876b7613ed9b987484
Rashmika Siriwardhana <58144396+rnsrashmika078@users.noreply.github.com>
2026-09-14 15:56:33 +0530

    order change

--- application/views/home.php
+++ application/views/home.php
@@ -10,25 +10,26 @@
     <?php $this->load->view('home/partials/navbar_v2'); ?>
 
     <main id="main-content">
-    <?php $this->load->view('home/sections/hero'); ?>
-    <?php $this->load->view('home/sections/statistics'); ?>
+        <?php $this->load->view('home/sections/hero'); ?>
+        <?php $this->load->view('home/sections/map'); ?>
 
-    <?php $this->load->view('home/sections/map'); ?>
-
-
-    <?php $this->load->view('home/sections/news'); ?>
-
-    <?php $this->load->view('home/sections/instrument'); ?>
-
-    <?php $this->load->view('home/sections/technicians'); ?>
+        <?php $this->load->view('home/sections/statistics'); ?>
 
 
 
-    <?php $this->load->view('home/sections/stakeholders'); ?>
+        <?php $this->load->view('home/sections/news'); ?>
 
-    <?php $this->load->view('home/sections/popups'); ?>
+        <?php $this->load->view('home/sections/instrument'); ?>
 
-    <?php $this->load->view('home/sections/disclaimer'); ?>
+        <?php $this->load->view('home/sections/technicians'); ?>
+
+
+
+        <?php $this->load->view('home/sections/stakeholders'); ?>
+
+        <?php $this->load->view('home/sections/popups'); ?>
+
+        <?php $this->load->view('home/sections/disclaimer'); ?>
     </main>
 
     <?php $this->load->view('home/partials/footer'); ?>
